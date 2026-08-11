5226217f132e777c44b170f7f2486d559e5b008e
gustavokra <gustavo.kraemer6@gmail.com>
2026-08-11 18:35:45 -0300

    retirando imports nulos

--- src/main/java/com/kraemer/tarefas/Entities/Etapa.java
+++ src/main/java/com/kraemer/tarefas/Entities/Etapa.java
@@ -1,7 +1,5 @@
 package com.kraemer.tarefas.Entities;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
@@ -11,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 
 @Entity(name = "etapa")
 public class Etapa {
--- src/main/java/com/kraemer/tarefas/Entities/Tarefa.java
+++ src/main/java/com/kraemer/tarefas/Entities/Tarefa.java
@@ -1,7 +1,5 @@
 package com.kraemer.tarefas.Entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
